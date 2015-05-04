af879b924ac1d9b4ad17de62af8b5d9106ccdc4e
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2015-05-04 16:29:15 +0530

    msm8952_64: Copy sound trigger mixer paths from msm8952_32
    Copy sound trigger mixer path file sound_trigger_mixer_paths_wcd9330.xml
    from 8952_32 for tomtom codec.
    
    Change-Id: Icb10bb382236478d8c112b53c16a2daa3b8f42b8

--- msm8952_64.mk
+++ msm8952_64.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8952_32/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/qcom/msm8952_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     device/qcom/msm8952_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    device/qcom/msm8952_32/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
     device/qcom/msm8952_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
     device/qcom/msm8952_64/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
