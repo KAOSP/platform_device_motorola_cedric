d9c657d776f30bbd6d3cf8357ac53d0964caa4fc
Preetam Singh Ranawat <apranawat@codeaurora.org>
2015-05-07 18:17:42 +0530

    msm8952_64: Audio: Copy audio_platform_info.xml from msm8952_32
     -copy audio_platform_info.xml from 8952_32 for
      24 bit playback support on speaker.
    
    Change-Id: I2b98490e17bf72ff51eba3022509f85000cd6aaf

--- msm8952_64.mk
+++ msm8952_64.mk
@@ -59,6 +59,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8952_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     device/qcom/msm8952_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
     device/qcom/msm8952_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    device/qcom/msm8952_32/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/qcom/msm8952_64/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
 
