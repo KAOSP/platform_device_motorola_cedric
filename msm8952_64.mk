fca08e7691752381aa21c5fa008583d667960717
padarshr <padarshr@codeaurora.org>
2015-06-16 14:31:36 +0530

    Remove 'nosdcard' PRODUCT_CHARACTERISTICS
    Since 8952 supports sdcard, this change is to
    remove the nosdcard Product Characteristic.
    
    CRs-Fixed: 846214
    
    Change-Id: I45bb2d7f3de9abae4a0e6777c0429ba5be9c5106

--- msm8952_64.mk
+++ msm8952_64.mk
@@ -36,9 +36,6 @@ PRODUCT_BOOT_JARS += qcmediaplayer \
 
 PRODUCT_BOOT_JARS += vcard
 
-# default is nosdcard, S/W button enabled in resource
-PRODUCT_CHARACTERISTICS := nosdcard
-
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
