ebe391c8a65f039225e62599a97a970b447e603a
Shantanu Jain <shjain@codeaurora.org>
2015-11-02 12:57:52 +0530

    thorium_64: Add kl file for QPNP PON driver
    Key layout gives android function mappings for kernel key codes.
    This adds the kl file for QPNP PON driver.
    
    Change-Id: I3659c8015b10e7064ba715ea9d4984f37143efb9

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -97,6 +97,14 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := qpnp_pon.kl
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
+
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
