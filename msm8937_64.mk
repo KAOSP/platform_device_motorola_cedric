cbc87bee498394fb6aefe0e46ee8e9fbd7d6c246
Prachee Ramsinghani <pracheer@codeaurora.org>
2016-02-12 15:21:28 +0530

    msm8937_64: Add gamedwhitelist.xml, push it /system/etc
    gamedwhitelist.xml contains the list of apps that have been
    whitelisted or blacklisted for gamed-daemon.
    The list of apps is specific to each target.
    Add gamedwhitelist.xml for each target and push it to
    /system/etc on the target.
    
    Change-Id: Ib748fa588935b9c45e8054ba99cd06d491cd1dd5

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -18,7 +18,8 @@ PRODUCT_COPY_FILES += device/qcom/msm8937_32/media/media_profiles_8937.xml:syste
                       device/qcom/msm8937_32/media/media_codecs_performance_8937.xml:system/etc/media_codecs_performance.xml
 endif
 
-PRODUCT_COPY_FILES += device/qcom/msm8937_64/whitelistedapps.xml:system/etc/whitelistedapps.xml
+PRODUCT_COPY_FILES += device/qcom/msm8937_64/whitelistedapps.xml:system/etc/whitelistedapps.xml \
+                      device/qcom/msm8937_64/gamedwhitelist.xml:system/etc/gamedwhitelist.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
            dalvik.vm.heapminfree=4m \
