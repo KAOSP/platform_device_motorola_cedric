6de3f7b1eaf240f89b903f927f92155feb73eece
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2016-07-13 13:11:03 +0530

    msm8937_64: removing non-existent libraries
    Removing non-existent library LibNimsWrap.so
    CRs-Fixed: 1033957
    
    Change-Id: Id2e439338484eb560e7493f03717a35af78a8461

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,8 +104,6 @@ TARGET_INIT_VENDOR_LIB := libinit_msm
 #add suffix variable to uniquely identify the board
 TARGET_BOARD_SUFFIX := _64
 
-TARGET_LDPRELOAD := libNimsWrap.so
-
 #Enable SSC Feature
 TARGET_USES_SSC := true
 
