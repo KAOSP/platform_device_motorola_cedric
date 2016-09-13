ab687e4baaca5477ee9c4575f1a60f85d10020e0
Uma Mehta <umamehta@codeaurora.org>
2016-09-13 11:52:54 +0530

    msm8937_64:  Allow mediacodec and mediaextractor syscalls
    Add select, eventfd, sendto, ecvfrom and
    other syscalls to make file operations
    successfull.
    
    Change-Id: I889bc25ac6cb5430dedbf6db32afa9f4578c0906

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,8 @@ TARGET_NO_RPC := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
 #BOARD_KERNEL_SEPARATED_DT := true
 
+BOARD_SECCOMP_POLICY := device/qcom/msm8937_32/seccomp
+
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
