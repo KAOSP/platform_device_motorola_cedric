f738110d1f469fae802823171169387b1f522b33
Bharat Pawar <bpawar@codeaurora.org>
2017-05-29 12:32:07 +0530

    msm8937_64: Move selinux to enforcing mode
    Moving selinux to enforcing mode.
    
    Change-Id: I9223e42c64d8a4083cd8e7d795d4016b597909a5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 TARGET_NO_RPC := true
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
 #BOARD_KERNEL_SEPARATED_DT := true
 
 BOARD_SECCOMP_POLICY := device/qcom/msm8937_32/seccomp
