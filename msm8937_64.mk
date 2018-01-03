fb99b15dcf60ac36e6443a28376a2012d9ce2d44
Anand Prusty <aprust@codeaurora.org>
2018-01-03 14:58:29 +0530

    msm8937_64 :Do not enable broadcastradio by default
    - Remove broadcast radio entries from manifest.xml
      and msm8937_64.mk.
    
    Change-Id: Ie55a5d612ea8c2b84518e2f8acdac2dfc1b18ec1

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -133,8 +133,7 @@ PRODUCT_PACKAGES += \
      android.hardware.memtrack@1.0-service \
      android.hardware.light@2.0-impl \
      android.hardware.light@2.0-service \
-     android.hardware.configstore@1.0-service \
-     android.hardware.broadcastradio@1.0-impl
+     android.hardware.configstore@1.0-service
 
 PRODUCT_PACKAGES += wcnss_service
 
