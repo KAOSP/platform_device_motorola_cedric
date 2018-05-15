f89df4cbb15414d7c3021a65ac89bccf2b310fbc
infixremix <solomonbooth63@gmail.com>
2018-05-15 15:50:02 +0000

    Cedric : Force Zram To be used (Will appear in memory)
    Signed-off-by: infixremix <solomonbooth63@gmail.com>
    This Should improve the 200mb free with users using xposed and multi tasking

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay 
 
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.config.zram=true
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
